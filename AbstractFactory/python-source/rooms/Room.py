e9287c1bbc21b2e8a1f855e059a5c7b8e3f2c81d
ashat1701 <ashat1701@github.com>
2019-05-12 18:46:17 +0300

    initial commit

--- AbstractFactory/python-source/rooms/Room.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Room(ABC):
-    @abstractmethod
-    def generate_boss(self) -> str:
-        pass
-
-    # virtual std::string generate_boss() = 0;
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Executing programs"
-LD_LIBRARY_PATH = "$LD_LIBRARY_PATH;./../lib-arm" ../bin-arm/AbstractFactory
-LD_LIBRARY_PATH = "$LD_LIBRARY_PATH;./../lib-arm" ../bin-arm/AbstractFactorySerializer
-LD_LIBRARY_PATH = "$LD_LIBRARY_PATH;./../lib-arm" ../bin-arm/Bridge
-LD_LIBRARY_PATH = "$LD_LIBRARY_PATH;./../lib-arm" ../bin-arm/Builder
-LD_LIBRARY_PATH = "$LD_LIBRARY_PATH;./../lib-arm" ../bin-arm/Facade
-LD_LIBRARY_PATH = "$LD_LIBRARY_PATH;./../lib-arm" ../bin-arm/Prototype
-
