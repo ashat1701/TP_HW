5a2fb4c682bbc3a08e5937817e84a600579be01e
ashat1701 <ashat1701@github.com>
2019-05-12 23:50:13 +0300

    delete old project

--- points.cpp
+++
@@ -1,9 +0,0 @@
-#include <cmath>
-#include "points.h"
-
-Point::Point(double x, double y): x(x), y(y) {}
-
-double dist(const Point &a, const Point &b) {
-    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
-}
-
--- points.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class Point {
-    double x, y;
-    friend double dist(const Point &a, const Point &b);
-    public:
-    Point(double x, double y);
-};
-
-double dist(const Point &a, const Point &b);
